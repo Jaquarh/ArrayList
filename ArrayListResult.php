a667c3da2f85511d815c9571a071d4d3c4fb6ff5
Kyle Jeynes <40298132+Jaquarh@users.noreply.github.com>
2018-12-08 15:56:13 +0000

    initial upload

---
+++ ArrayListResult.php
@@ -0,0 +1,26 @@
+final class ArrayListResult
+{
+	private $result;
+
+	public function __construct( $array )
+	{
+		if( !is_array( $array ) )
+			throw new Exception('Parameter one expects an array datatype.');
+		
+		$this->result = $array;
+	}
+
+	private function isPresent()
+	{
+		return !empty( $this->result );
+	}
+	
+	public function getFirstOrDefault() {
+		return new ArrayList( $this->isPresent() ? $this->result[0] : [] );	
+	}
+	
+	public function getResults() {
+		return new ArrayList( $this->isPresent() ? $this->result : [] );	
+	}
+	
+}
