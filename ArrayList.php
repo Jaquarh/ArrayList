f557d494abbfc1a1d2d7122ebf92c1e4b3fa7be5
Kyle Jeynes <40298132+Jaquarh@users.noreply.github.com>
2018-12-08 15:55:46 +0000

    initial upload

---
+++ ArrayList.php
@@ -0,0 +1,48 @@
+final class ArrayList
+{
+	private $array;
+
+	public function __construct( $array )
+	{
+		if( !is_array( $array ) )
+			throw new Exception('Parameter one expects an array datatype.');
+		$this->array = $array;
+	}
+	
+	public function add($value, $key = "") {
+		if( empty( $key ) ) {
+			$this->array[] = $value;
+			return $this;
+		}
+		$this->array[$key] = $value;
+		return $this;
+	}
+
+	public function where( $closure )
+	{
+		if( !is_callable( $closure ) )
+			throw new Exception('Parameter one expects a closure datatype.');
+
+		return new ArrayListResult( array_filter($this->array, $closure) );
+	}
+	
+	public function get() {
+		return $this->array;
+	}
+	
+	public function isPresent()
+	{
+		return !empty( $this->array );
+	}
+
+	public function ifPresent( $closure )
+	{
+		if( !is_callable( $closure ) )
+			throw new Exception('Parameter one expects a closure datatype.');
+
+		if( $this->isPresent() )
+		{
+			call_user_func_array( $closure, $this->array );
+		}
+	}
+}
