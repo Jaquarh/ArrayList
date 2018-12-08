883da1cb8f00d1a0e9dfd916645c3c1c96dc5b7b
Kyle Jeynes <40298132+Jaquarh@users.noreply.github.com>
2018-12-08 19:09:29 +0000

    Update travis.php

--- test/travis.php
+++ test/travis.php
@@ -1,6 +1,6 @@
-require_once '../ArrayList.php';
+require_once '../src/ArrayList.php';
 
-$myArray = new ArrayList([
+$myArray = new Jaquarh\ArrayList\ArrayList([
     ['id' => 1, 'username' => 'foo', 'privilidges' => ['can_read' => 1, 'can_write' => 0], 'incomming' => ''],
     ['id' => 2, 'username' => 'bar', 'privilidges' => ['can_read' => 1, 'can_write' => 1], 'incomming' => 'Hi everyone!']
 ]);
